fb4b72deeab7f09fafb406bb9df72ee01bf31908
Vitaliy Bykovets <vitaliy.bykovets@swiftpage.com>
2020-07-20 22:43:54 +0300

    feat: add recaptcha and nodemailer

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-
-app.use(express.json());
+const nodemailer = require("nodemailer");
 
 require('dotenv').config();
+app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+});
+
+const mailOptions = (from, text) => ({
+  from,
+  to: process.env.EMAIL,
+  subject: 'Contact Form Portfolio - Message',
+  text
+});
 
 app.post('/contact', function (req, res) {
-  console.log('********* - 6', req.body);
-  res.send(req.body)
+  const { email, message } = req.body;
+  const mailOption = mailOptions(email, message);
+  transporter.sendMail(mailOption, (err, data) => {
+    if (err) {
+      return res.send({ status: 'error', error: err });
+    }
+    return res.send({ status: 'success' });
+  });
 });
 
 if (!module.parent) {
